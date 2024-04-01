c60a38d8983b06ea022898e476bb090df6bb309b
MariaProcopii <mari.procopii@gmail.com>
2024-04-01 10:27:19 +0300

    Included all extra points

--- go2web.py
+++ go2web.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import os
 import re
 import ssl
 import sys
 import socket
-import subprocess 
+import hashlib
+import subprocess
 
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
@@ -33,7 +35,7 @@ class Parser:
             encoding="utf-8",
             errors="ignore",
         ).stdout
-    
+
     def parse_html_links(self, data):
         soup = BeautifulSoup(data, "html.parser")
         dom = etree.HTML(str(soup))
@@ -47,17 +49,50 @@ class HTTPHandler:
     def __init__(self):
         self.search_link = "https://www.google.com/search?q={}"
         self.search_path = "/search?q={}"
+        self.cache_path = "./cache"
         self.parser = Parser()
-    
+
+        try:
+            if not os.path.exists(self.cache_path):
+                os.mkdir(self.cache_path)
+        except Exception as e:
+            print(f">> An unexpected error occured: {e}")
+
+    def search_file(self, filename):
+        for root, dirs, files in os.walk(self.cache_path):
+            if filename in files:
+                return os.path.join(root, filename)
+        return ""
+
+    def get_filename(self, *args):
+        hash_func = hashlib.sha256()
+        for arg in args:
+            if not isinstance(arg, str):
+                arg = str(arg)
+            hash_func.update(arg.encode("utf-8"))
+        return hash_func.hexdigest()
+
     def request(self, host, port, path):
-        response = "" 
+        filename = self.get_filename(host, port, path)
+        if self.search_file(filename):
+            print(">> Cache hit.")
+            try:
+                with open(f"{self.cache_path}/{filename}", "r") as fp:
+                    headers, body = fp.read().split("DELIMITER")
+                    return [headers, body]
+            except Exception as e:
+                print(f">> An unexpected error occured: {e}")
+                if os.path.exists(f"{self.cache_path}/{filename}"):
+                    os.remove(f"{self.cache_path}/{filename}")
+
+        response = ""
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, port))
 
         if port == HTTPS_PORT:
             ctx = ssl.create_default_context()
             sock = ctx.wrap_socket(sock, server_hostname=host)
-        
+
         sock.sendall(
             (f"GET {path} HTTP/1.1\r\n" +
              f"Host: {host}\r\n" +
@@ -71,7 +106,7 @@ class HTTPHandler:
             data = sock.recv(4096)
             if not data: break
             response += data.decode("utf-8")
-        
+
         sock.close()
 
         headers, body = response.split("\r\n\r\n", 1)
@@ -83,13 +118,31 @@ class HTTPHandler:
                 port = HTTP_PORT
             else:
                 port = HTTPS_PORT
-            
+
             return self.request(host, port, path)
-        
+
+        if not self.search_file(filename):
+            print(">> Cache miss. Saving the url")
+            try:
+                with open(f"{self.cache_path}/{filename}", "w") as fp:
+                    fp.write(headers)
+                    fp.write("DELIMITER")
+                    fp.write(body)
+            except Exception as e:
+                print(f">> An unexpected error occured: {e}")
+
         return [headers, body]
-    
+
     def search(self, queries):
         search_query = '+'.join(queries)
+        filename = self.get_filename(search_query)
+        if self.search_file(filename):
+            print(">> Cache hit.")
+            try:
+                with open(f"{self.cache_path}/{filename}", "r") as fp:
+                    body = fp.read()
+            except Exception as e:
+                print(f">> An unexpected error occured: {e}")
 
         port = HTTPS_PORT
         path = self.search_path.format(search_query)
@@ -98,6 +151,15 @@ class HTTPHandler:
         ).netloc
 
         _, body = self.request(host, port, path)
+
+        if not self.search_file(filename):
+            print(">> Cache miss. Saving the url")
+            try:
+                with open(f"{self.cache_path}/{filename}", "w") as fp:
+                    fp.write(body)
+            except Exception as e:
+                print(f">> An unexptected error occured: {e}")
+
         return self.parser.parse_html_links(body)
 
 if __name__=="__main__":
@@ -109,7 +171,7 @@ if __name__=="__main__":
             port = HTTP_PORT
         else:
             port = HTTPS_PORT
-        
+
         header, body = http_handler.request(host, port, path)
         print(http_handler.parser.parse_html_page(body))
 
@@ -117,7 +179,7 @@ if __name__=="__main__":
         links = http_handler.search(sys.argv[2:])
         for idx, link in enumerate(links, 1):
             print(f"{idx}. {link}")
-        
+
         option = input("Select the number of the link you want to access or enter `q` to quit\nOption >> ")
         if option == "q":
             sys.exit()
@@ -127,11 +189,11 @@ if __name__=="__main__":
                 port = HTTP_PORT
             else:
                 port = HTTPS_PORT
-            
+
             header, body = http_handler.request(host, port, path)
             print(http_handler.parser.parse_html_page(body))
         else:
-            print("Wrong option")
+            print(">> Wrong option")
             sys.exit()
 
     else:
